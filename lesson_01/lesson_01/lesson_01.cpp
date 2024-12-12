15de7e7c44a7fc1f30d119f99383c781c5c89f06
LandSStudio <35688813+LandS-Studio@users.noreply.github.com>
2024-12-12 19:24:10 +0200

    Add lesson_01 homework

---
+++ lesson_01/lesson_01/lesson_01.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "I love C++!\n";
+
+    std::cout << "*\n";
+    std::cout << "**\n";
+    std::cout << "* *\n";
+    std::cout << "*  *\n";
+    std::cout << "*   *\n";
+    std::cout << "*    *\n";
+    std::cout << "*     *\n";
+    std::cout << "*      *\n";
+    std::cout << "*       *\n";
+    std::cout << "*        *\n";
+    std::cout << "* * * * * *\n";
+}
