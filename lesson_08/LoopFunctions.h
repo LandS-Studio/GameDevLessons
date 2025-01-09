8ca2e89a172344ce6c36f6c938b362efc4ca46cb
LandSStudio <35688813+LandS-Studio@users.noreply.github.com>
2025-01-09 17:16:53 +0200

    Create lesson_08.cpp

---
+++ lesson_08/LoopFunctions.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// For STEP 1
+// Function for counting numbers
+void countNumbers(int count);
+
+// For STEP 2
+// The function of calculating the factorial using a for
+int factorialIterative(int count);
+
+// For STEP 4
+// Print numbers from largest to smallest using a loop
+void printNumbersLargeToSmallLoop(int count);
+
+// Print numbers from smallest to largest using a loop
+void printNumbersSmallToLargeLoop(int count);
---
+++ lesson_08/RecursiveFunctions.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For STEP 2
+// Function for calculating the factorial using recursion
+int factorialRecursive(int count);
+
+// For STEP 4
+// Print numbers from largest to smallest using recursion
+void printNaturalNumbersLargeToSmallRecursion(int count);
